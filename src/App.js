c36cf8754b2b056ef68928be83445732b1dec157
kumari-shikha <shikha.kumari@capillarytech.com>
2024-06-05 13:32:19 +0530

    template

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [counter, setCounter] = useState(0);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <button id="increment" onClick={() => setCounter(count => count+1)}>Increment counter</button>
+      <button id="decrement" onClick={() => setCounter(count => count >0 ? count-1 : count)}>Decrement counter</button>
+      <h4 id="counter">Count {counter}</h4>
     </div>
   );
 }
