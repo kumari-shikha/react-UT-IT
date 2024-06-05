612c2f9ef862694893976df5d608e44e69b82f42
kumari-shikha <shikha.kumari@capillarytech.com>
2024-06-05 17:06:06 +0530

    full release

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ root.render(
   </React.StrictMode>
 );
 
-//It's a full release from master
+//Full master release 2
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
